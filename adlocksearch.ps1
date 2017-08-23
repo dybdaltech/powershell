bd004d4dae8a715dd24bcac850bb9adec2273da2
Benjamin Andre Dybdal <bad@nesna.lokal>
2017-08-23 09:51:56 +0200

    refined it a bit

--- adlocksearch.ps1
+++ adlocksearch.ps1
@@ -2,11 +2,8 @@ $gruppe = Get-ADgroupMember -Identity "Locked out gruppe"
 
 foreach($person in $gruppe){
     $identity = $person.SamAccountName
-    $a = Get-Aduser $identity
-    $b = $a | Select-Object LockedOut
-    if ($b = "@{LockedOut=True}") {
-        Write-Host "true"
-    } else { 
-        Write-Host "False"
+    $a = $identity | select LockedOut
+    if($a = "@{LockedOut=True}"){
+        Write-Host "Account Locked out: " + $identity.DistuingishedName
     }
 }
