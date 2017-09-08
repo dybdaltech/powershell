b2b448bc5536cc780818d6e16703797689eb7b00
Benjamin Andre Dybdal <bad@nesna.lokal>
2017-09-08 12:57:14 +0200

    Added support for HTTPS

--- file.ps1
+++ file.ps1
@@ -2,6 +2,7 @@ $a = $true
 if ($a -eq $true) {
     Add-Type -AssemblyName System.Speech
      $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
      $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'https://catfact.ninja/fact')).fact
      $SpeechSynth.Speak("did you know?")
     $SpeechSynth.Speak($CatFact)
