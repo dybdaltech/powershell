6bc911958d674afbb769e9b8996586c3cea4928f
Benjamin Andre Dybdal <bad@nesna.lokal>
2017-09-15 12:49:00 +0200

    updated

--- findingandsettingAD.ps1
+++ findingandsettingAD.ps1
@@ -1,32 +1,37 @@
-#Lag et random passord!
+#Create a random password!
 function createPassword () {
     $store = Get-Random -Minimum 1000 -Maximum 9999
-    $pass = "nesna" + $store.ToString()
+    $pass = "Password" + $store.ToString()
     Write-Host $pass
     Write-Output $pass | Out-File "F:\Benjamin\out.txt"
     $securePass = ConvertTo-SecureString -String $pass -AsPlainText -Force
     return $securePass
 }
 
+#Find the ADUser
 function find-aduser([string]$firstName, [string]$lastName){
     $storeUser = "*$firstName* *$lastName*"
     $aduser = Get-ADUser -f {name -like $storeUser}
+    return $aduser
 }
+
+
 $a = $true
 Write-Host "STARTED, Write name."
 While($a -eq $true) {
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Red
     $userInput = Read-Host 
     if( $userInput -eq "exit" ) {
+        Write-Host "Exiting.."
+        Start-sleep -Seconds 1
         $a = $false
     } else {
         Write-Host "Searching AD..."
         $search = find-aduser $userInput
-        Start-Sleep -Seconds 1
         write-host $search.SAMAccountName
         $newPassword = createPassword
         Set-AdAccountPassword -Identity $search -Reset -NewPassword $newPassword
-        Write-Host "Reload.."
+        Write-Host "Reloading.."
     }
 }
 
