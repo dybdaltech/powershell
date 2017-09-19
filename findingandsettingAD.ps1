e8534f596894823cad3eddd1fb7e4f3ec5d89bb3
Benjamin Andre Dybdal <bad@nesna.lokal>
2017-09-19 12:09:53 +0200

    tyjrtyjrthtrhy

--- findingandsettingAD.ps1
+++ findingandsettingAD.ps1
@@ -3,9 +3,10 @@ function createPassword () {
     $store = Get-Random -Minimum 1000 -Maximum 9999
     $pass = "Password" + $store.ToString()
     Write-Host $pass
-    Write-Output $pass | Out-File "F:\Benjamin\out.txt"
-    $securePass = ConvertTo-SecureString -String $pass -AsPlainText -Force
-    return $securePass
+    Write-Output $pass | Out-File "E:\Benjamin\out.txt"
+    #$securePass = ConvertTo-SecureString -String $pass -AsPlainText -Force
+    #return $securePass
+    return $pass
 }
 
 #Find the ADUser
@@ -30,6 +31,9 @@ While($a -eq $true) {
         $search = find-aduser $userInput
         write-host $search.SAMAccountName
         $newPassword = createPassword
+        Write-Output $search.SAMAccountName + "|" + $newPassword | clip 
+        Write-Output $search.SAMAccountName + "|" + $newPassword | Out-File "E:\Benjamin\outTotal.txt"
+        $newPassword = ConvertTo-SecureString -String $pass -AsPlainText -Force
         Set-AdAccountPassword -Identity $search -Reset -NewPassword $newPassword
         Write-Host "Reloading.."
     }
