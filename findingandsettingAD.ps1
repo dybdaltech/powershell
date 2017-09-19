0fee8509defada725bc4e99a12f2e3f951117621
Benjamin Andre Dybdal <bad@nesna.lokal>
2017-09-19 15:21:20 +0200

    asdwdqwdw

--- findingandsettingAD.ps1
+++ findingandsettingAD.ps1
@@ -1,7 +1,7 @@
 #Create a random password!
 function createPassword () {
     $store = Get-Random -Minimum 1000 -Maximum 9999
-    $pass = "Password" + $store.ToString()
+    $pass = "NesnaPassord" + $store.ToString()
     Write-Host $pass
     Write-Output $pass | Out-File "E:\Benjamin\out.txt"
     #$securePass = ConvertTo-SecureString -String $pass -AsPlainText -Force
@@ -11,8 +11,18 @@ function createPassword () {
 
 #Find the ADUser
 function find-aduser([string]$firstName, [string]$lastName){
-    $storeUser = "$firstName * $lastName"
+    $storeUser = "$firstName" + "*" + "$lastName"
     $aduser = Get-ADUser -Filter {name -like $storeUser}
+    if($aduser -isnot [Microsoft.ActiveDirectory.Management.ADAccount]){
+        write-host "Multiple users found: " $aduser.length
+        $choice = Read-Host 
+        if($choice -gt $aduser.length){
+            Write-Host "Number greater than array length, please choose a number between 0 and " $aduser.length
+            return
+        }
+        Write-Host $aduser[$choice]
+        return $aduser[$choice]
+    }
     Write-Host $aduser
     return $aduser
 }
@@ -23,12 +33,12 @@ Write-Host "STARTED, Write name."
 While($a -eq $true) {
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Red
     $userInput = Read-Host 
-    Write-host $userInput+"1"
-    $search = find-aduser $userInput
+    $badinput = Read-Host
+    $search = find-aduser $userInput $badinput
     $newPassword = createPassword
-    Write-Output $search.SAMAccountName + "|" + $newPassword | clip 
-    Write-Output $search.SAMAccountName + "|" + $newPassword | Out-File "E:\Benjamin\outTotal.txt"
-    $newPassword = ConvertTo-SecureString -String $pass -AsPlainText -Force -ErrorAction SilentlyContinue 
+    #Write-Output $search.SAMAccountName + "|" + $newPassword | clip 
+    Write-Output $search.SAMAccountName "|" $newPassword | Out-File "E:\Benjamin\outTotal.txt" -NoNewline -Append
+    $newPassword = ConvertTo-SecureString -String $newPassword.ToString() -AsPlainText -Force -ErrorAction SilentlyContinue 
     Set-AdAccountPassword -Identity $search -Reset -NewPassword $newPassword -ErrorAction SilentlyContinue
     Write-Host "Reloading.."
 }
