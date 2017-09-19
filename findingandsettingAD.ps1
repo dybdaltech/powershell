b6805608316a357736aae25b17ecde4c99c6180d
Benjamin Andre Dybdal <bad@nesna.lokal>
2017-09-19 13:23:21 +0200

    wdrf

--- findingandsettingAD.ps1
+++ findingandsettingAD.ps1
@@ -11,8 +11,9 @@ function createPassword () {
 
 #Find the ADUser
 function find-aduser([string]$firstName, [string]$lastName){
-    $storeUser = "*$firstName* *$lastName*"
-    $aduser = Get-ADUser -f {name -like $storeUser}
+    $storeUser = "$firstName * $lastName"
+    $aduser = Get-ADUser -Filter {name -like $storeUser}
+    Write-Host $aduser
     return $aduser
 }
 
@@ -22,20 +23,13 @@ Write-Host "STARTED, Write name."
 While($a -eq $true) {
     Write-Host "-------------------------------------------------------------------" -ForegroundColor Red
     $userInput = Read-Host 
-    if( $userInput -eq "exit" ) {
-        Write-Host "Exiting.."
-        Start-sleep -Seconds 1
-        $a = $false
-    } else {
-        Write-Host "Searching AD..."
-        $search = find-aduser $userInput
-        write-host $search.SAMAccountName
-        $newPassword = createPassword
-        Write-Output $search.SAMAccountName + "|" + $newPassword | clip 
-        Write-Output $search.SAMAccountName + "|" + $newPassword | Out-File "E:\Benjamin\outTotal.txt"
-        $newPassword = ConvertTo-SecureString -String $pass -AsPlainText -Force
-        Set-AdAccountPassword -Identity $search -Reset -NewPassword $newPassword
-        Write-Host "Reloading.."
-    }
+    Write-host $userInput+"1"
+    $search = find-aduser $userInput
+    $newPassword = createPassword
+    Write-Output $search.SAMAccountName + "|" + $newPassword | clip 
+    Write-Output $search.SAMAccountName + "|" + $newPassword | Out-File "E:\Benjamin\outTotal.txt"
+    $newPassword = ConvertTo-SecureString -String $pass -AsPlainText -Force -ErrorAction SilentlyContinue 
+    Set-AdAccountPassword -Identity $search -Reset -NewPassword $newPassword -ErrorAction SilentlyContinue
+    Write-Host "Reloading.."
 }
 
